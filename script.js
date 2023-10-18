c28b0536df2dd5a8763d7a9f9ee042b084e9ceb4
Priest-0811 <142845519+Priest-0811@users.noreply.github.com>
2023-10-18 13:00:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array.shift())
+
+console.log(array.sort())
+
+console.log(array.push("kiwi"))
+
+console.log(array.reverse())
+
+console.log(array.pop())
